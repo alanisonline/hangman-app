3bbf8b8b98f2bfabf3008a220f106a1ec426aa3d
Alan <>
2021-04-19 16:38:59 +0100

    Minor refactoring

--- quasar-app/src/pages/Index.vue
+++ quasar-app/src/pages/Index.vue
@@ -1,58 +1,58 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <!-- <example-component
-      title="Example component"
-      active
-    ></example-component> -->
     <div v-if="user">
       <p>{{ this.user.hasOwnProperty('name') ? this.user.name : '' }}</p>
       <GameComponent />
     </div>
     <div v-else>
-      <q-card square bordered class="q-pa-lg shadow-1">
-        <q-card-section>
-          <q-form
-            class="q-gutter-md"
-            method="POST"
-            @submit="handleLogin"
-            action="/login"
-          >
-            <q-input
-              square
-              class="full-width"
-              v-model="login.email"
-              type="email"
-              label="email"
-            />
-            <q-input
-              square
-              filled
-              clearable
-              class="full-width"
-              v-model="login.password"
-              type="password"
-              label="password"
-            />
-            <q-btn
-              unelevated
-              color="light-green-7"
-              size="lg"
-              class="full-width"
-              label="Login"
-              type="submit"
-            />
-          </q-form>
-        </q-card-section>
-        <q-card-actions class="q-px-md"> </q-card-actions>
-        <q-card-section class="text-center q-pa-none">
-          <p class="text-grey-6">
-            Not registered?
-            <q-btn flat class="text-blue-3" v-on:click="$emit('register')"
-              >Create an Account</q-btn
+      <div class="column">
+        <div class="row">
+          <h5 class="text-h5 text-grey-8 q-my-md">Login</h5>
+        </div>
+        <q-card square bordered class="q-pa-lg shadow-1">
+          <q-card-section class="form-card-section">
+            <q-form
+              class="q-gutter-md full-width"
+              method="POST"
+              @submit="handleLogin"
+              action="/login"
             >
-          </p>
-        </q-card-section>
-      </q-card>
+              <q-input
+                square
+                class="full-width"
+                v-model="login.email"
+                type="email"
+                label="email"
+              />
+              <q-input
+                square
+                filled
+                clearable
+                class="full-width"
+                v-model="login.password"
+                type="password"
+                label="password"
+              />
+              <q-btn
+                unelevated
+                color="light-green-7"
+                size="lg"
+                class="full-width"
+                label="Login"
+                type="submit"
+              />
+            </q-form>
+          </q-card-section>
+          <q-card-section hide class="text-center">
+            <p class="text-grey-6">
+              Not registered?
+              <q-btn flat class="text-blue-3" v-on:click="$emit('register')"
+                >Create an Account</q-btn
+              >
+            </p>
+          </q-card-section>
+        </q-card>
+      </div>
     </div>
   </q-page>
 </template>
@@ -87,9 +87,7 @@ export default Vue.extend({
       void axios
         .get('api/user')
         .then((response) => {
-          console.log(response.data);
           this.user = <unknown | null>response.data;
-          console.log(this.user);
         })
         .catch((err) =>
           console.log('There has been an error with getting User', err)
@@ -106,3 +104,9 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style lang="scss">
+.form-card-section {
+  min-width: 320px;
+}
+</style>
